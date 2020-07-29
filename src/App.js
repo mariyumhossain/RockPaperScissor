d4aa6ca1a2caa4030e993d705b76b9a059cba4c3
Mariyum Hossain <mariyumhossain@Mariyums-MacBook-Air.local>
2020-07-29 15:14:47 -0400

    game working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import PlayerCard from "./PlayerCard.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Game extends Component{
+    constructor(){
+      super()
+      this.sign = ["rock","scissors","paper"]
+      this.state = {
+        playerOne: "rock",
+        playerTwo: "scissors"
+      }
+    }
+
+
+playGame = () => {
+  this.setState({
+    playerOne: this.sign[Math.floor(Math.random()*3)],
+    playerTwo: this.sign[Math.floor(Math.random()*3)]
+  })
 }
 
-export default App;
+decideWinner = () => {
+  const playerOne = this.state.playerOne
+  const playerTwo = this.state.playerTwo
+  if(playerOne === playerTwo){
+    return "It's a tie!"
+  } else if ((playerOne === "rock" && playerTwo === "scissors")
+  ||(playerOne === "paper" && playerTwo === "rock")
+  ||(playerOne === "scissors" && playerTwo === "paper")){
+    return "Player 1 Wins!"
+  }else{
+    return "Player 2 Wins!"
+  }
+}
+
+  render(){
+    return (
+      <div className="style">
+        <div>
+          <PlayerCard sign={this.state.playerOne}></PlayerCard>
+          <PlayerCard sign={this.state.playerTwo}></PlayerCard>
+        </div>
+    <div className="winner">{this.decideWinner()}</div>
+        <button className="button" onClick={this.playGame}>Play the Game</button>
+      </div>
+    )
+  }
+}
+
+export default Game
